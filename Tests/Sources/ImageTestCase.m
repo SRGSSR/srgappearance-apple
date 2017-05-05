5ccf00f7edfae26732140a3681b6cbfcd6e33fdf
Samuel Défago <defagos@gmail.com>
2017-05-05 16:07:26 +0200

    Add test case file

---
+++ Tests/Sources/ImageTestCase.m
@@ -0,0 +1,18 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <SRGAppearance/SRGAppearance.h>
+#import <XCTest/XCTest.h>
+
+@interface ImageTestCase : XCTestCase
+
+@end
+
+@implementation ImageTestCase
+
+
+
+@end
