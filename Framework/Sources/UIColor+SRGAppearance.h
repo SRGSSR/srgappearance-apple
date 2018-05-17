90e04e601d0e5df1ce717631abadc5cde5fb87e0
Samuel Défago <defagos@gmail.com>
2018-05-17 14:04:48 +0200

    Add color hexadecimal transformations

--- Framework/Sources/UIColor+SRGAppearance.h
+++ Framework/Sources/UIColor+SRGAppearance.h
@@ -8,6 +8,12 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ *  Transformer between hexadecimal strings and colors. Convert valid hexadecimal strings with or without leading
+ *  wildcards to colors. Conversely, produces hexadecimal strings from colors (always with leading wildcard):
+ */
+OBJC_EXPORT NSValueTransformer *SRGHexadecimalColorTransformer(void);
+
 @interface UIColor (SRGAppearance)
 
 /**
@@ -19,6 +25,12 @@ NS_ASSUME_NONNULL_BEGIN
  *  The official SRG SSR blue color.
  */
 + (UIColor *)srg_blueColor;
+
+/**
+ *  Return the color matching a hexadecimal string (with or without leading wildcard), `nil` if the string does
+ *  not correspond to a color.
+ */
++ (nullable UIColor *)srg_colorFromHexadecimalString:(NSString *)hexadecimalString;
 
 @end
 
--- Framework/Sources/UIColor+SRGAppearance.m
+++ Framework/Sources/UIColor+SRGAppearance.m
@@ -6,16 +6,83 @@
 
 #import "UIColor+SRGAppearance.h"
 
+@interface SRGHexadecimalColorValueTransformer : NSValueTransformer
+
+@end
+
 @implementation UIColor (SRGAppearance)
 
 + (UIColor *)srg_redColor
 {
-    return [UIColor colorWithRed:157.f / 255.f green:0.f / 255.f blue:24.f / 255.f alpha:1.f];
+    return [UIColor srg_colorFromHexadecimalString:@"#9d0018"];
 }
 
 + (UIColor *)srg_blueColor
 {
-    return [UIColor colorWithRed:15.f / 255.f green:90.f / 255.f blue:203.f / 255.f alpha:1.f];
+    return [UIColor srg_colorFromHexadecimalString:@"#0f5acb"];
+}
+
++ (UIColor *)srg_colorFromHexadecimalString:(NSString *)hexadecimalString
+{
+    return [SRGHexadecimalColorTransformer() transformedValue:hexadecimalString];
 }
 
 @end
+
+@implementation SRGHexadecimalColorValueTransformer
+
+#pragma mark Overrides
+
++ (Class)transformedValueClass
+{
+    return [UIColor class];
+}
+
++ (BOOL)allowsReverseTransformation
+{
+    return YES;
+}
+
+- (id)transformedValue:(id)value
+{
+    if (! [value isKindOfClass:[NSString class]]) {
+        return nil;
+    }
+    
+    NSScanner *scanner = [NSScanner scannerWithString:value];
+    if ([value hasPrefix:@"#"]) {
+        [scanner setScanLocation:1];
+    }
+    
+    unsigned rgbValue = 0;
+    if (! [scanner scanHexInt:&rgbValue]) {
+        return nil;
+    }
+    
+    CGFloat red = ((rgbValue & 0xFF0000) >> 16) / 255.f;
+    CGFloat green = ((rgbValue & 0x00FF00) >> 8) / 255.f;
+    CGFloat blue = (rgbValue & 0x0000FF) / 255.f;
+    return [UIColor colorWithRed:red green:green blue:blue alpha:1.f];
+}
+
+- (id)reverseTransformedValue:(id)value
+{
+    if (! [value isKindOfClass:[UIColor class]]) {
+        return nil;
+    }
+    
+    const CGFloat *components = CGColorGetComponents([value CGColor]);
+    return [NSString stringWithFormat:@"#%02lx%02lx%02lx", lroundf(components[0] * 255), lroundf(components[1] * 255), lroundf(components[2] * 255)];
+}
+
+@end
+
+NSValueTransformer *SRGHexadecimalColorTransformer(void)
+{
+    static NSValueTransformer *s_transformer;
+    static dispatch_once_t s_onceToken;
+    dispatch_once(&s_onceToken, ^{
+        s_transformer = [[SRGHexadecimalColorValueTransformer alloc] init];
+    });
+    return s_transformer;
+}
